4827646566c80fce6c733edbc44e48da44a8e695
egg <egg95@protonmail.com>
2022-09-18 13:05:07 -0700

    updated main

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,12 @@ use plumbus;
 #[clap(author = "R.Egg <egg95@protonmail.com>")]
 #[clap(version = "1.0.3")]
 #[clap(about = "Copy the output of a command to your clipboard. 
-    Use the --file option to copy the contents of a file", long_about = None)]
+Use the --file option to copy the contents of a file
+
+usage: echo 'hello' | plumbus ", long_about = None)]
 struct Args {
     /// Copy the contents of a file
-    /// plumbus --file hello.txt
+    /// plumbus -f hello.txt
     #[clap(short, long, value_parser, verbatim_doc_comment, default_value_t)]
     file: String,
 }
@@ -19,9 +21,12 @@ fn main() {
     let args = Args::parse();
 
     if atty::is(Stream::Stdin) {
-        plumbus::copy_file_clipboard(args.file);
+        if args.file != String::new() {
+            plumbus::copy_file_clipboard(args.file);
+        }
         return;
     }
+
     let stdin = std::io::stdin();
     plumbus::copy_to_clipboard(stdin);
 }
