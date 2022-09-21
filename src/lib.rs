dd718f1fc9af08e6c7df8916aa36944bdc1e23f7
Radical Egg <luther.jaymen@gmail.com>
2022-09-21 14:47:28 -0700

    added verbose option and error prop

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,27 @@
 use clipboard::{ClipboardContext, ClipboardProvider};
 use std::io::{self, BufRead};
-use std::panic;
 
-pub fn copy_to_clipboard(stdin: io::Stdin) {
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
+pub fn copy_to_clipboard(stdin: io::Stdin) -> Result<String, Box<dyn std::error::Error>>  {
+    let mut ctx: ClipboardContext = ClipboardProvider::new()?;
     let mut lines = stdin
         .lock()
         .lines()
-        .fold(String::new(), |accum, line| accum + &line.unwrap() + "\n");
+        .fold(String::new(), 
+            |accum, line| accum + &line.unwrap() + "\n");
     lines.pop();
 
-    ctx.set_contents(lines).unwrap();
-    ctx.get_contents().unwrap();
+    ctx.set_contents(lines)?;
+    let content = ctx.get_contents()?;
+    Ok(content)
 }
 
-pub fn copy_file_clipboard(fp: String) {
-    let contents = std::fs::read_to_string(fp);
+pub fn copy_file_clipboard(fp: String) -> Result<(), Box<dyn std::error::Error>> {
+    let contents = std::fs::read_to_string(fp)?;
 
-    match contents {
-        Ok(content) => {
-            let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-            ctx.set_contents(content).unwrap();
-            ctx.get_contents().unwrap();
-        }
-        Err(err) => {
-            panic::set_hook(Box::new(|info| {
-                println!("{}", info);
-            }));
-            panic!("{}", err);
-        }
-    };
+    let mut ctx: ClipboardContext = ClipboardProvider::new()?;
+
+    ctx.set_contents(contents)?;
+    ctx.get_contents()?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use atty::Stream;
 use clap::Parser;
 use plumbus;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 #[clap(name = "Plumbus")]
 #[clap(author = "R.Egg <egg95@protonmail.com>")]
-#[clap(version = "1.0.3")]
+#[clap(version = "1.0.4")]
 #[clap(about = "Copy the output of a command to your clipboard. 
 Use the --file option to copy the contents of a file
 
@@ -16,17 +16,38 @@ struct Args {
     /// plumbus -f hello.txt
     #[clap(short, long, value_parser, verbatim_doc_comment, default_value_t)]
     file: String,
+
+    /// Use the verbose flag to show output
+    /// echo "hello world!" | plumbus -v
+    #[clap(short, long, verbatim_doc_comment)]
+    verbose: bool,
 }
 fn main() {
     let args = Args::parse();
 
     if atty::is(Stream::Stdin) {
         if args.file != String::new() {
-            plumbus::copy_file_clipboard(args.file);
+            match plumbus::copy_file_clipboard(args.file).err() {
+                Some(err) => {
+                    println!("{}", err.to_string());
+                    return
+                },
+                None => return
+            }
         }
         return;
     }
 
     let stdin = std::io::stdin();
-    plumbus::copy_to_clipboard(stdin);
+
+    match plumbus::copy_to_clipboard(stdin) {
+        Ok(content) => {
+            if args.verbose {
+                println!("{}", content);
+            } 
+        },
+        Err(err) => {
+            println!("{}", err.to_string());
+        }
+    }
 }
